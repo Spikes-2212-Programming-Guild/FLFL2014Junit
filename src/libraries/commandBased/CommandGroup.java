27c31b09b71d8662a1d452c9427fb13656470ca7
Thinkredstone <ittai11@gmail.com>
2014-11-11 15:02:39 +0200

    added execute and needed methods

--- src/libraries/commandBased/CommandGroup.java
+++ src/libraries/commandBased/CommandGroup.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package libraries.commandBased;
 
 import java.util.ArrayList;
@@ -12,24 +11,38 @@ import java.util.ArrayList;
  *
  * @author 207797739
  */
-public class CommandGroup extends Command{
+public class CommandGroup extends Command {
+
     private ArrayList<ArrayList<Command>> commands = new ArrayList<>();
     private int currentCommandBlock = 0;
-    private int runningBlock=-1;
-    
-    protected void addParallel(Command command){
+    //-1 for no block is running
+    private int runningBlock = -1;
+
+    protected void addParallel(Command command) {
         commands.get(currentCommandBlock).add(command);
     }
-    protected void addSequential(Command command){
+
+    protected void addSequential(Command command) {
         addParallel(command);
         commands.add(new ArrayList<>());
         currentCommandBlock++;
     }
-    
-    private void runBlock(){
-        
+
+    private void runBlock(int blockNum) {
+        runningBlock = blockNum;
+        for (Command c : commands.get(blockNum)) {
+            c.start();
+        }
     }
-    
+
+    private boolean blockFinished(int blockNum) {
+        if (blockNum > -1) {
+            return commands.get(blockNum).get(commands.get(blockNum).size() - 1).isFinished();
+        } else {
+            return true;
+        }
+    }
+
     @Override
     protected void initialize() {
         commands.add(new ArrayList<>());
@@ -37,20 +50,25 @@ public class CommandGroup extends Command{
 
     @Override
     protected void execute() {
-        
+        if (blockFinished(runningBlock)) {
+            runningBlock++;
+            runBlock(runningBlock);
+        }
     }
 
     @Override
     protected void end() {
+        runningBlock = -1;
     }
 
     @Override
     protected void interrupted() {
+        end();
     }
 
     @Override
     protected boolean isFinished() {
         return false;
     }
-    
+
 }
