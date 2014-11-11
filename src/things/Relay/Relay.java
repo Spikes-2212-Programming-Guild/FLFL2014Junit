155599207094090759a19729950bad843035da6f
eyal <eyal@eyal-debian>
2014-11-11 18:27:56 +0200

    added talon and relay

---
+++ src/things/Relay/Relay.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Relay;
+
+/**
+ *
+ * @author Developer
+ */
+public class Relay {
+    
+    private Value value;
+    
+    public Relay(int port){
+    }
+    
+    public void set(Value value){
+        this.value = value;
+    }
+    
+    public static class Value {
+        public static Value kOn;
+        public static Value kOff;
+        public static Value kForward;
+        public static Value kReverse;
+    }
+    
+    public Value get() {
+        return value;
+    }
+        
+}
