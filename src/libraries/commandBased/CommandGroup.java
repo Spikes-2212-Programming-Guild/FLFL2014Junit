d29052847b461768816d278024052063666d39c1
ThinkRedstone <ittai11@gmail.com>
2014-11-17 10:48:15 +0200

    added comments

--- src/libraries/commandBased/CommandGroup.java
+++ src/libraries/commandBased/CommandGroup.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class CommandGroup extends Command {
 
     private ArrayList<ArrayList<Command>> commands = new ArrayList<>();
+    //what command block are we currently building
     private int currentCommandBlock = 0;
     //-1 for no block is running
     private int runningBlock = -1;
