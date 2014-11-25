092bdd52eea648d69d17cdc0b029f521bc4518a2
ThinkRedstone <ittai11@gmail.com>
2014-11-25 16:28:24 +0200

    Modefied relay to work with unit testing

--- src/libraries/relay/Relay.java
+++ src/libraries/relay/Relay.java
@@ -9,25 +9,36 @@ package libraries.relay;
  * @author Developer
  */
 public class Relay {
-    
+
     private Value value;
-    
-    public Relay(int port){
+
+    public Relay(int port) {
     }
-    
-    public void set(Value value){
+
+    public void set(Value value) {
         this.value = value;
     }
-    
+
     public static class Value {
-        public static Value kOn;
-        public static Value kOff;
-        public static Value kForward;
-        public static Value kReverse;
+
+        private int v;
+
+        public Value(int v) {
+            this.v = v;
+        }
+
+        public int getV() {
+            return v;
+        }
+
+        public static Value kOn = new Value(2);
+        public static Value kOff = new Value(0);
+        public static Value kForward = new Value(1);
+        public static Value kReverse = new Value(-1);
     }
-    
+
     public Value get() {
         return value;
     }
-        
+
 }
